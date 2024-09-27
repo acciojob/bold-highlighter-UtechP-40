02936984da6e1977bd57bd883b965d19bbd3d895
Pradeep Pandey <90238672+UtechP-40@users.noreply.github.com>
2024-09-27 22:50:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 function highlight() {
     //Write your code here
-
+    let text = document.querySelectorAll("strong");
+    for(let t of text){
+        t.style.color = "green";
+    }
 
 }
 
 
 function return_normal() {
     //Write your code here
-
+    let text = document.querySelectorAll("strong");
+    for(let t of text){
+        t.style.color = "black";
+    }
     
 }
